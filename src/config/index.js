c83498483232e819ee680d713bdaffc31735c3df
panyue <pyyybf@gmail.com>
2023-07-14 09:40:23 +0800

    add base url of deployed backend

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : '';
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://backend-dot-automated-dataset-generation.wl.r.appspot.com';
