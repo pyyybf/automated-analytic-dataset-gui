dd4aef454147ca99c9ba262c7aeee7e616ca66c8
panyue <pyyybf@gmail.com>
2023-09-12 03:02:50 -0700

    add account and assignment reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import {createStore, combineReducers} from 'redux';
 
 import {generatorReducer} from "./generator/generator.reducer";
+import {accountReducer} from "@/store/account/account.reducer";
+import {assignmentReducer} from "@/store/assignment/assignment.reducer";
 
 export default createStore(
     combineReducers({
         generator: generatorReducer,
+        account: accountReducer,
+        assignment: assignmentReducer,
     })
 );
