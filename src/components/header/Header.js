f976abb198a3f88e058bc3808c3e6b73e72046f5
panyue <pyyybf@gmail.com>
2023-09-12 03:03:53 -0700

    add tab to change route

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,83 @@
+import React from 'react';
 import './Header.css';
-import {IconButton, TextField} from "@mui/material";
+import {Button, IconButton, Tab, Tabs} from "@mui/material";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import {useDispatch, useSelector} from "react-redux";
-import {setNumberOfRows} from "@/store/generator/generator.action";
+import {setAlert} from "@/store/generator/generator.action";
+import {ALERT_DURATION, HEADER_HEIGHT} from "@/config";
+import {Link, useLocation, useNavigate} from 'react-router-dom';
+import {useEffect, useState} from "react";
+import LoginDialog from "@/components/header/components/loginDialog/LoginDialog";
+import {setShowLoginDialog} from "@/store/account/account.action";
+import SelfMenu from "@/components/header/components/selfMenu/SelfMenu";
 
 export default function Header() {
     const dispatch = useDispatch();
-    const numberOfRows = useSelector(state => state.generator.numberOfRows);
+    const token = useSelector(state => state.account.token);
+
+    const [tab, setTab] = useState('/');
+
+    const location = useLocation();
+    const navigate = useNavigate();
+    useEffect(() => {
+        setTab(location.pathname);
+        // check auth
+        if (location.pathname === '/editor') {
+            if (token !== 'TA' && token !== 'INSTRUCTOR') {
+                dispatch(setAlert(true, 'Please sign in!'));
+                setTimeout(() => {
+                    dispatch(setAlert(false));
+                }, ALERT_DURATION);
+                navigate('/');
+            }
+        } else if (location.pathname === '/dashboard') {
+            if (token !== 'INSTRUCTOR') {
+                dispatch(setAlert(true, 'Please sign in!'));
+                setTimeout(() => {
+                    dispatch(setAlert(false));
+                }, ALERT_DURATION);
+                navigate('/');
+            }
+        }
+    }, [location, token, dispatch, navigate]);
+
+    const onSignIn = () => {
+        dispatch(setShowLoginDialog(true));
+        navigate('/');
+    };
 
     return (
         <header>
             <div className="header-logo">
-                {/*<h2 className="header-title">Predictor Matrix Definition</h2>*/}
                 <img src={require('@/assets/autogen-logo.png')} alt="Autogen Logo"/>
+                <Tabs centered
+                      sx={{marginLeft: '28px'}}
+                      value={tab}
+                      onChange={(e, newVal) => {
+                          setTab(newVal);
+                      }}>
+                    {token !== 'TA' && token !== 'INSTRUCTOR' ? null :
+                        <Tab value="/"
+                             component={Link}
+                             label="Home"
+                             to="/"
+                             sx={{height: HEADER_HEIGHT}}/>}
+                    {token !== 'TA' && token !== 'INSTRUCTOR' ? null :
+                        <Tab value="/editor"
+                             component={Link}
+                             label="Editor"
+                             to="/editor"
+                             sx={{height: HEADER_HEIGHT}}/>}
+                    {token !== 'INSTRUCTOR' ? null :
+                        <Tab value="/dashboard"
+                             component={Link}
+                             label="Dashboard"
+                             to="/dashboard"
+                             sx={{height: HEADER_HEIGHT}}/>}
+                </Tabs>
             </div>
             <div className="header-item">
-                <TextField label="Number of Rows"
-                           size="small"
-                           type="number"
-                           value={numberOfRows}
-                           onChange={e => {
-                               dispatch(setNumberOfRows(e.target.value));
-                           }}/>
+                {token ? <SelfMenu/> : <Button variant="outlined" onClick={onSignIn}>Sign in</Button>}
                 <IconButton aria-label="github"
                             color="primary"
                             target="_blank"
@@ -29,6 +85,7 @@ export default function Header() {
                     <GitHubIcon/>
                 </IconButton>
             </div>
+            <LoginDialog/>
         </header>
     );
 }
