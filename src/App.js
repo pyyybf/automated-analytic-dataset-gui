9235c818191033a00ea5a8c9d920e715ac16eb5c
panyue <pyyybf@gmail.com>
2023-09-12 03:00:30 -0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import './App.css';
 import {Alert, Grid, Snackbar} from "@mui/material";
 import Header from "./components/header/Header";
-import Options from "./components/options/Options";
-import Graph from "./components/graph/Graph";
-import UniqueIdentifierDialog from "./components/uniqueIdentifierDialog/UniqueIdentifierDialog";
-import MultivariateNormalDialog from "./components/multivariateNormalDialog/MultivariateNormalDialog";
-import UniformDialog from "./components/uniformDialog/UniformDialog";
-import BetaDialog from "./components/betaDialog/BetaDialog";
-import CategoricalDialog from "./components/categoricalDialog/CategoricalDialog";
-import CategoricalToNumericalDialog from "./components/categoricalToNumericalDialog/CategoricalToNumericalDialog";
-import MulticollinearDialog from "./components/multicollinearDialog/MulticollinearDialog";
-import PolynomialCategoricalDialog from "./components/polynomialCategoricalDialog/PolynomialCategoricalDialog";
-import ResponseVectorDialog from "./components/responseVectorDialog/ResponseVectorDialog";
-import CodeDialog from "./components/codeDialog/CodeDialog";
-import {useSelector} from "react-redux";
 import {HEADER_HEIGHT} from "./config";
+import Editor from "@/components/editor/Editor";
+import {
+    Routes,
+    Route,
+} from "react-router-dom";
+import React from "react";
+import {useSelector} from "react-redux";
+import Home from "@/components/home/Home";
+import Dashboard from "@/components/dashboard/Dashboard";
 
 function App() {
     const alertOpen = useSelector(state => state.generator.alertOpen);
@@ -34,36 +30,16 @@ function App() {
                     <Header/>
                 </Grid>
                 <Grid item xs={12}>
-                    <Grid container sx={{height: `calc(100vh - ${HEADER_HEIGHT}px)`}}>
-                        <Grid item xs={12} md={3} sx={{
-                            padding: '16px 28px',
-                            height: '100%',
-                            overflowY: 'scroll',
-                        }}>
-                            <Options/>
-                        </Grid>
-                        <Grid item xs={12} md={9} sx={{
-                            height: '100%',
-                            overflowY: 'scroll',
-                        }}>
-                            <Graph/>
-                        </Grid>
-                    </Grid>
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/editor" element={<Editor/>}/>
+                        <Route path="/dashboard" element={<Dashboard/>}/>
+                    </Routes>
                 </Grid>
             </Grid>
             <Snackbar open={alertOpen} anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
                 <Alert severity={alertType}>{alertText}</Alert>
             </Snackbar>
-            <UniqueIdentifierDialog/>
-            <MultivariateNormalDialog/>
-            <UniformDialog/>
-            <BetaDialog/>
-            <CategoricalDialog/>
-            <CategoricalToNumericalDialog/>
-            <MulticollinearDialog/>
-            <PolynomialCategoricalDialog/>
-            <ResponseVectorDialog/>
-            <CodeDialog/>
         </div>
     );
 }
