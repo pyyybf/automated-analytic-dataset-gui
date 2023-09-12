88174c3575b569df523e1a707d85193b7501269c
panyue <pyyybf@gmail.com>
2023-09-12 03:04:43 -0700

    add instructor dashboard

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,134 @@
+import {ALERT_DURATION, HEADER_HEIGHT} from "@/config";
+import {
+    Button,
+    ButtonGroup,
+    Chip,
+    CircularProgress,
+    Grid,
+    IconButton,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+} from "@mui/material";
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import React, {useEffect, useState} from "react";
+import {setAlert} from "@/store/generator/generator.action";
+import {
+    getAllAccounts,
+    setAccountId,
+    setShowChangePwdDialog,
+    setShowConfirmDeleteAccountDialog
+} from "@/store/account/account.action";
+import {useDispatch} from "react-redux";
+import ChangePwdDialog from "@/components/dashboard/components/changePwdDialog/ChangePwdDialog";
+import ConfirmDeleteDialog from "@/components/dashboard/components/confirmDeleteDialog/ConfirmDeleteDialog";
+
+export default function Dashboard() {
+    const dispatch = useDispatch();
+
+    const [accounts, setAccounts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const HOME_COLOR_TBL = {
+        INSTRUCTOR: 'warning',
+        TA: 'primary',
+    };
+
+    const getAccountList = () => {
+        setLoading(true);
+        getAllAccounts().then(res => {
+            setAccounts(res);
+        }).catch(err => {
+            dispatch(setAlert(true, err));
+            setTimeout(() => {
+                dispatch(setAlert(false));
+            }, ALERT_DURATION);
+        }).finally(() => {
+            setLoading(false);
+        });
+    };
+
+    const handleChangePwd = (id) => {
+        dispatch(setAccountId(id));
+        dispatch(setShowChangePwdDialog(true));
+    };
+
+    const handleDelete = (id) => {
+        dispatch(setAccountId(id));
+        dispatch(setShowConfirmDeleteAccountDialog(true));
+    };
+
+    // get account list from backend
+    useEffect(() => {
+        getAccountList();
+    }, [dispatch]);
+
+    return (
+        <React.Fragment>
+            <Grid container
+                  direction="row"
+                  justifyContent="center"
+                  alignItems="flex-start"
+                  sx={{height: `calc(100vh - ${HEADER_HEIGHT}px)`, overflowY: 'scroll'}}>
+                <Grid item xs={12} sm={10} md={8} sx={{marginTop: '28px'}}>
+                    <TableContainer component={Paper}>
+                        <Table sx={{minWidth: 650}} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell sx={{fontWeight: 'bold'}}>Username</TableCell>
+                                    <TableCell align="center" sx={{fontWeight: 'bold'}}>Last Name</TableCell>
+                                    <TableCell align="center" sx={{fontWeight: 'bold'}}>First Name</TableCell>
+                                    <TableCell align="center" sx={{fontWeight: 'bold'}}>Role</TableCell>
+                                    <TableCell align="center" sx={{fontWeight: 'bold'}}></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {loading ?
+                                    <TableRow>
+                                        <TableCell colSpan={5} sx={{textAlign: 'center'}}>
+                                            <CircularProgress/>
+                                        </TableCell>
+                                    </TableRow>
+                                    : accounts.map((account, index) => (
+                                        <TableRow key={index}
+                                                  sx={{'&:last-child td, &:last-child th': {border: 0}}}>
+                                            <TableCell component="th" scope="row">
+                                                {account.username}
+                                            </TableCell>
+                                            <TableCell align="center">{account.lastName}</TableCell>
+                                            <TableCell align="center">{account.firstName}</TableCell>
+                                            <TableCell align="center">
+                                                <Chip size="small"
+                                                      variant="outlined"
+                                                      label={account.role}
+                                                      color={HOME_COLOR_TBL[account.role]}/>
+                                            </TableCell>
+                                            <TableCell align="center">
+                                                <ButtonGroup variant="text">
+                                                    <Button color="primary" onClick={() => {
+                                                        handleChangePwd(account._id);
+                                                    }}>Change password</Button>
+                                                    <IconButton color="error"
+                                                                onClick={() => {
+                                                                    handleDelete(account._id);
+                                                                }}>
+                                                        <DeleteOutlineOutlinedIcon fontSize="small"/>
+                                                    </IconButton>
+                                                </ButtonGroup>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+            </Grid>
+            <ChangePwdDialog/>
+            <ConfirmDeleteDialog onConfirm={getAccountList}/>
+        </React.Fragment>
+    );
+}
---
+++ src/components/dashboard/components/changePwdDialog/ChangePwdDialog.js
@@ -0,0 +1,88 @@
+import {
+    Button,
+    Dialog, DialogActions,
+    DialogContent,
+    DialogTitle,
+    TextField
+} from "@mui/material";
+import {useDispatch, useSelector} from "react-redux";
+import {setShowChangePwdDialog, updatePassword} from "@/store/account/account.action";
+import {useState} from "react";
+import {setAlert} from "@/store/generator/generator.action";
+import {ALERT_DURATION} from "@/config";
+
+const sha256 = require("js-sha256").sha256;
+
+export default function ChangePwdDialog() {
+    const dispatch = useDispatch();
+    const showChangePwdDialog = useSelector(state => state.account.showChangePwdDialog);
+    const accountId = useSelector(state => state.account.accountId);
+
+    const [oldPassword, setOldPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleCloseDialog = () => {
+        setOldPassword('');
+        setNewPassword('');
+        setConfirmPassword('');
+        dispatch(setShowChangePwdDialog(false));
+    };
+    const handleSubmit = () => {
+        if (newPassword !== confirmPassword) {
+            dispatch(setAlert(true, 'Password confirmation doesn\'t match the password.', 'warning'));
+            setTimeout(() => {
+                dispatch(setAlert(false, 'Password confirmation doesn\'t match the password.', 'warning'));
+            }, ALERT_DURATION);
+            return;
+        }
+        updatePassword(accountId, sha256(oldPassword), sha256(newPassword)).then(res => {
+            dispatch(setAlert(true, 'Password changed successfully.', 'success'));
+            setTimeout(() => {
+                dispatch(setAlert(false, 'Password changed successfully.', 'success'));
+            }, ALERT_DURATION);
+            handleCloseDialog();
+        }).catch(err => {
+            dispatch(setAlert(true, err));
+            setTimeout(() => {
+                dispatch(setAlert(false));
+            }, ALERT_DURATION);
+        });
+    };
+
+    return (
+        <Dialog open={showChangePwdDialog} onClose={handleCloseDialog} maxWidth="xs">
+            <DialogTitle>Change Password</DialogTitle>
+            <DialogContent>
+                <TextField fullWidth
+                           sx={{marginTop: '12px'}}
+                           type="password"
+                           label="Old password"
+                           value={oldPassword}
+                           onChange={e => {
+                               setOldPassword(e.target.value);
+                           }}/>
+                <TextField fullWidth
+                           sx={{marginTop: '12px'}}
+                           label="New password"
+                           type="password"
+                           value={newPassword}
+                           onChange={e => {
+                               setNewPassword(e.target.value);
+                           }}/>
+                <TextField fullWidth
+                           sx={{marginTop: '12px'}}
+                           label="Confirm new password"
+                           type="password"
+                           value={confirmPassword}
+                           onChange={e => {
+                               setConfirmPassword(e.target.value);
+                           }}/>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleSubmit}>Submit</Button>
+                <Button onClick={handleCloseDialog}>Cancel</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
