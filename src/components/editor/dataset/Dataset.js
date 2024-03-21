3cc3f083d9a86665603a6ac585a0218b487b572c
pyyybf <pyyybf@gmail.com>
2024-03-20 21:36:40 -0700

    move dialog files

--- src/components/editor/dataset/Dataset.js
+++ src/components/editor/dataset/Dataset.js
@@ -4,16 +4,16 @@ import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
 import {HEADER_HEIGHT} from "@/config";
 import Options from "./components/options/Options";
 import Graph from "./components/graph/Graph";
-import UniqueIdentifierDialog from "./components/uniqueIdentifierDialog/UniqueIdentifierDialog";
-import MultivariateNormalDialog from "./components/multivariateNormalDialog/MultivariateNormalDialog";
-import UniformDialog from "./components/uniformDialog/UniformDialog";
-import BetaDialog from "./components/betaDialog/BetaDialog";
-import CategoricalDialog from "./components/categoricalDialog/CategoricalDialog";
-import CategoricalToNumericalDialog from "./components/categoricalToNumericalDialog/CategoricalToNumericalDialog";
-import MulticollinearDialog from "./components/multicollinearDialog/MulticollinearDialog";
-import PolynomialCategoricalDialog from "./components/polynomialCategoricalDialog/PolynomialCategoricalDialog";
-import ResponseVectorDialog from "./components/responseVectorDialog/ResponseVectorDialog";
-import CodeDialog from "./components/codeDialog/CodeDialog";
+import UniqueIdentifierDialog from "./components/dialogs/uniqueIdentifierDialog/UniqueIdentifierDialog";
+import MultivariateNormalDialog from "./components/dialogs/multivariateNormalDialog/MultivariateNormalDialog";
+import UniformDialog from "./components/dialogs/uniformDialog/UniformDialog";
+import BetaDialog from "./components/dialogs/betaDialog/BetaDialog";
+import CategoricalDialog from "./components/dialogs/categoricalDialog/CategoricalDialog";
+import CategoricalToNumericalDialog from "./components/dialogs/categoricalToNumericalDialog/CategoricalToNumericalDialog";
+import MulticollinearDialog from "./components/dialogs/multicollinearDialog/MulticollinearDialog";
+import PolynomialCategoricalDialog from "./components/dialogs/polynomialCategoricalDialog/PolynomialCategoricalDialog";
+import ResponseVectorDialog from "./components/dialogs/responseVectorDialog/ResponseVectorDialog";
+import CodeDialog from "./components/dialogs/codeDialog/CodeDialog";
 import {useNavigate} from "react-router-dom";
 
 export default function Dataset() {
